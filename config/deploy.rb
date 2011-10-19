683ae022ecf432dbf1abd6af77f16f4a2498d3d9
Richard Nghiem <richard@zendesk.com>
2011-10-19 01:39:36 +0000

    cap basic deployable

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,20 @@ role :web, "richard2"
 role :app, "richard2"
 role :db,  "richard2", :primary => true
 
+set :deploy_to, "/data/graphite"
+set :user, "zendesk"
+set :runner, "zendesk"
+
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  task :permissions do
+    run "sudo chown -Rfv #{user}.#{user} #{deploy_to}"
+  end
 end
+
+after  "deploy:setup","deploy:permissions"
